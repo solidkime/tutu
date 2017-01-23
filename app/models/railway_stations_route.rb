c48720e7cb74c7f65f8fdb1c3656324f5ad814eb
solidkime <dr.kime@gmail.com>
2017-01-23 17:27:40 +0200

    task 13 done

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,5 +1,5 @@
 class RailwayStationsRoute < ActiveRecord::Base
-  # Значит, что у нас есть рейлвейл_стейшн_айди и роут_айди
+  # Значит, что у нас есть рейлвей_стейшн_айди и роут_айди
   belongs_to :railway_station
   belongs_to :route
 end
