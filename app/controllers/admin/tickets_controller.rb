02acaa3db0b9ddf55f2c8b4ae65ebd40872ab526
solidkime <dr.kime@gmail.com>
2017-02-21 00:11:35 +0200

    fixed admin

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,36 +1,38 @@
 class Admin::TicketsController < Admin::BaseController
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   def new
     @ticket = Ticket.new
     #@user = User.new
   end
 
-  def create
-    #@user = User.create(name: params[:user_name])
-    @ticket = current_user.tickets.new(ticket_params)
-
-    if @ticket.save
-      redirect_to @ticket
-    else
-      render :new
-    end
-  end
-
   def show
-    @ticket = Ticket.find(params[:id])
   end
 
   def index
     @tickets = Ticket.all
   end
 
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to [:admin, @ticket], notice: 'Ticket was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @ticket = Ticket.find(params[:id])
     @ticket.destroy
     redirect_to tickets_url, notice: 'Ticket was successfully deleted.'
   end
 
-
   private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 
   def ticket_params
     params.require(:ticket).permit(:user_name, :fio, :passport, :start_station_id, :end_station_id, :train_id, :number)
