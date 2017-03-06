070feac081a27ed90007ec7f43baa94b2699ce47
solidkime <dr.kime@gmail.com>
2017-03-06 12:14:40 +0200

    Lesson 20 added case to wagons.js

--- app/assets/javascripts/wagons.js
+++ app/assets/javascripts/wagons.js
@@ -1,29 +1,53 @@
 $(document).ready(function(){
   $('#wagon_type').change(function(){
    var type = "";
-   
-   $( "select option:selected" ).each(function() {
-      type += $( this ).text();
-    });
-   if (type === "EconomyWagon") {
-    $('.lux, .sitting, .coupe').addClass("hidden");
-    $('.economy').removeClass("hidden");
-  };
 
-  if (type === "CoupeWagon") {
+    $( "select option:selected" ).each(function() {
+       type += $( this ).text();
+     });
+  //  if (type === "EconomyWagon") {
+  //   $('.lux, .sitting, .coupe').addClass("hidden");
+  //   $('.economy').removeClass("hidden");
+  // };
+
+  // if (type === "CoupeWagon") {
+  //   $('.lux, .sitting, .economy').addClass("hidden");
+  //   $('.coupe').removeClass("hidden");
+  // };
+
+  // if (type === "SittingWagon") {
+  //   $('.lux, .economy, .coupe').addClass("hidden");
+  //   $('.sitting').removeClass("hidden");
+  // };
+
+  // if (type === "LuxWagon") {
+  //   $('.economy, .sitting, .coupe').addClass("hidden");
+  //   $('.lux').removeClass("hidden");
+  // };
+
+   switch(type){
+   case "EconomyWagon":
+   $('.lux, .sitting, .coupe').addClass("hidden");
+   $('.economy').removeClass("hidden");
+   break;
+  
+
+  case "CoupeWagon":
     $('.lux, .sitting, .economy').addClass("hidden");
     $('.coupe').removeClass("hidden");
-  };
+    break;
 
-  if (type === "SittingWagon") {
+
+  case "SittingWagon":
     $('.lux, .economy, .coupe').addClass("hidden");
     $('.sitting').removeClass("hidden");
-  };
+    break;
 
-  if (type === "LuxWagon") {
+  case "LuxWagon":
     $('.economy, .sitting, .coupe').addClass("hidden");
     $('.lux').removeClass("hidden");
-  };
+    break;
+};
 
 
   });
